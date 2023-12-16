ae3b6ba88107a40acfea00b74910bcafdd9c8468
Maksym <139382157+Shunpocode@users.noreply.github.com>
2023-12-16 12:05:33 +0300

    index.php

---
+++ index.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<link rel="stylesheet" href="css.css"/>
+<title>Paralaks</title>
+</head>
+	<body>
+		<header>
+			<a href="#" class="logo">Logo</a>
+			<ul>
+				<li><a href="#">Home</a></li>
+				<li><a href="#">About</a></li>
+				<li><a href="#">Work</a></li>
+				<li><a href="#">Contact</a></li>
+			</ul>
+		</header>
+		<section id="section">
+			<button><a href="#">Explore</a></button>
+		</section>
+		<a class="txt">
+			<h1>Parallacs</h1>
+			The Parallax Effect is a difference in the apparent position of an object when viewed along two different lines of sight. The Parallax it's measured by the semi-angle of inclination between those two lines.The Parallax Effect is a difference in the apparent position of an object when viewed along two different lines of sight. The Parallax it's measured by the semi-angle of inclination between those two lines.<br><br>The Parallax Effect is a difference in the apparent position of an object when viewed along two different lines of sight. The Parallax it's measured by the semi-angle of inclination between those two lines.<br><br>The Parallax Effect is a difference in the apparent position of an object when viewed along two different lines of sight. The Parallax it's measured by the semi-angle of inclination between those two lines.The Parallax Effect is a difference in the apparent position of an object when viewed along two different lines of sight. The Parallax it's measured by the semi-angle of inclination between those two lines.<br><br>The Parallax Effect is a difference in the apparent position of an object when viewed along two different lines of sight. The Parallax it's measured by the semi-angle of inclination between those two lines.<br><br>The Parallax Effect is a difference in the apparent position of an object when viewed along two different lines of sight. The Parallax it's measured by the semi-angle of inclination between those two lines.<br><br>The Parallax Effect is a difference in the apparent position of an object when viewed along two different lines of sight. The Parallax it's measured by the semi-angle of inclination between those two lines.<br><br>The Parallax Effect is a difference in the apparent position of an object when viewed along two different lines of sight. The Parallax it's measured by the semi-angle of inclination between those two lines.<br><br>The Parallax Effect is a difference in the apparent position of an object when viewed along two different lines of sight. The Parallax it's measured by the semi-angle of inclination between those two lines.<br><br>The Parallax Effect is a difference in the apparent position of an object when viewed along two different lines of sight. The Parallax it's measured by the semi-angle of inclination between those two lines.<br><br>The Parallax Effect is a difference in the apparent position of an object when viewed along two different lines of sight. The Parallax it's measured by the semi-angle of inclination between those two lines.<br><br>The Parallax Effect is a difference in the apparent position of an object when viewed along two different lines of sight. The Parallax it's measured by the semi-angle of inclination between those two lines.<br><br>The Parallax Effect is a difference in the apparent position of an object when viewed along two different lines of sight. The Parallax it's measured by the semi-angle of inclination between those two lines.<br><br>The Parallax Effect is a difference in the apparent position of an object when viewed along two different lines of sight. The Parallax it's measured by the semi-angle of inclination between those two lines.<br><br>
+		</a>
+		<script>
+			window.onresize = function(){section();}
+			window.onscroll = function(){section();}
+			function section(){
+			let posTop = window.scrollY;
+			if(posTop <= 1300){
+				document.getElementById('section').style.backgroundPosition ='50% 50%,'+ (735-(posTop*5)) + 'px 50%, 50% '+ (0+(posTop*0.6)) +'px, 50% '+ (0+(posTop*1.3)) +'px, '+ ((posTop*1)-0) +'px '+ (0-(posTop*1)) +'px';
+				}
+			}
+		</script>
+	</body>
+</html>
